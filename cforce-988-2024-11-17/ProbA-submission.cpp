e1a45726236e4f0c7556b39b517851778844088e
Ramana <patil_raman@yahoo.com>
2024-11-18 22:02:29 +0800

    codeforce contest 988

---
+++ cforce-988-2024-11-17/ProbA-submission.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int test(vector<int> &t)
+{
+    size_t n = t.size();
+    int cnt =0;
+
+    sort(t.begin(), t.end());
+
+    for(int i=0;i<n-1;i++)
+        if(t[i] == t[i+1])
+        {
+            cnt++;
+            i = i+1;
+        }
+    return cnt;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int tc,n,score=0;
+    cin>>tc;
+
+    for(int i=0;i<tc;i++)
+    {
+        cin>>n;
+        vector<int> a(n,0);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        cout<<test(a)<<'\n';
+        a.clear();
+    }
+}
---
+++ cforce-988-2024-11-17/ProbB-submission.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void test(vector<int> &t)
+{
+    size_t n = t.size();
+    int cnt =0;
+
+    //sort(t.begin(), t.end());
+    int nxm = n-2;
+    int fact1=1, fact2=nxm;
+    bool bfact1=false, bfact2=false;
+
+
+    for(int i=0;i<n;i++)
+    {
+        if(((i+1) * (nxm/(i+1))) != nxm) continue;
+        else
+        {
+            fact1=(i+1), fact2=nxm/(i+1);
+            bfact1=false;
+            bfact2=false;
+        }
+
+        for(int j=0;j<n;j++)
+        {
+            if(fact1 == t[j]) bfact1 = true;
+            if(fact2 == t[j]) bfact2 = true;
+
+            if((bfact1 == true)&&(bfact2 == true))
+            {
+                cout<<fact1<<" "<<fact2<<endl;
+                return;
+            }
+        }
+
+    }
+
+    return;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int tc,k,score=0;
+    cin>>tc;
+
+    for(int i=0;i<tc;i++)
+    {
+        cin>>k;
+        vector<int> a(k,0);
+        for(int i=0;i<k;i++)
+        {
+            cin>>a[i];
+        }
+        test(a);
+        a.clear();
+    }
+}
